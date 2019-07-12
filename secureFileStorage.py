d96b1937da739dd7102b3cbac0343996e0be04ca
Sarath Kumar Selvam <sarathkumarselvam@Saraths-MacBook-Air-10.local>
2019-07-12 06:49:25 +0530

    Working version 1.0 of Secure file storage

--- secureFileStorage.py
+++ secureFileStorage.py
@@ -1,15 +1,22 @@
 import random
 import time
+import sys
+import os
 from Crypto.Cipher import AES
 from Crypto.Cipher import DES
 from Crypto.Cipher import ARC4
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 file_length = 0
 split_length = 0
 current_split = 1
 count = 0
 algorithm = "aes"
-input_file_name = "input.txt"
+input_file_name = sys.argv[2]
 encrypted_file_name = "encrypted_file.txt"
 decrypted_file_name = "decrypted_file.txt"
 aes_key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
@@ -96,21 +103,67 @@ def decrypt_file(encrypted_file_name, decrypted_file):
 				if(current_split == 3 and file_length % 3 != 0):
 					split_length += file_length % 3
 
+def sendEmail(email_user, email_password, email_send):
+	subject = 'Key'
+
+	msg = MIMEMultipart()
+	msg['From'] = email_user
+	msg['To'] = email_send
+	msg['Subject'] = subject
+
+	body = 'Hi there, this is a key for hybrid decryption'
+	msg.attach(MIMEText(body,'plain'))
+
+	filename='key.txt'
+	attachment = open(filename, "rb")
+
+	part = MIMEBase('application','octet-stream')
+	part.set_payload((attachment).read())
+	encoders.encode_base64(part)
+	part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+	msg.attach(part)
+	text = msg.as_string()
+	server = smtplib.SMTP('smtp.gmail.com',587)
+	server.starttls()
+	server.login(email_user,email_password)
+
+
+	server.sendmail(email_user,email_send,text)
+	server.quit()
+	os.remove("key.txt")
+
 with open("input.txt") as f:
 	for line in f:
 		file_length += 1
 
-print "file length: ", file_length
 split_length = file_length / 3
-print "split length: ", split_length
 
-encrypted_file = open(encrypted_file_name, "w")
-encrypt_file(input_file_name, encrypted_file)
-
-encrypted_file = open(encrypted_file_name, "r")
-decrypted_file = open(decrypted_file_name, "w")
-algorithm = "aes"
-BS = 16
-count = 0
-current_split = 1
-decrypt_file(encrypted_file_name, decrypted_file)
+if(sys.argv[1] == "encrypt"):
+	encrypted_file = open(encrypted_file_name, "w")
+	encrypt_file(input_file_name, encrypted_file)
+	email_user = raw_input('Enter email id: ')
+	email_password = raw_input('Enter password: ')
+	email_send = raw_input('Enter recipient email id: ')
+	filename = 'key.txt'
+	attachment = open(filename, "w")
+	attachment.write(str(aes_key)+"256"+str(aes_iv)+"256"+str(des_key)+"256"+str(arc4_key))
+	attachment.close()
+	sendEmail(email_user, email_password, email_send)
+elif(sys.argv[1] == "decrypt"):
+	encrypted_file = open(encrypted_file_name, "r")
+	decrypted_file = open(decrypted_file_name, "w")
+	algorithm = "aes"
+	BS = 16
+	count = 0
+	current_split = 1
+	keys = ""
+	with open("key.txt") as f:
+		for line in f:
+			keys += line
+	keys = keys.split("256")
+	aes_key = keys[0]
+	aes_iv = keys[1]
+	des_key = keys[2]
+	arc4_key = keys[3]
+	decrypt_file(encrypted_file_name, decrypted_file)
